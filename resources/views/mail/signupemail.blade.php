6fa5ee4e419de2513a72bf75dad8ccef6f301a56
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2022-04-25 15:53:27 +0100

    controller and route update

---
+++ resources/views/mail/signupemail.blade.php
@@ -0,0 +1,13 @@
+Hello {{$email_data['name']}}
+<br><br>
+Welcome to my website
+<br>
+Please click the link below to activate your account
+<br><br>
+<a href="http://localhost/laravel_react/laravel_product_api/public/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+
+
+
+<br><br>
+Thank You
+<br>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/verify', [RegisterController::class, 'verifyUser'])->name('verify');
+
+//Route::get('/verify','Auth\RegisterController@verifyUser')->name('verify.user');
